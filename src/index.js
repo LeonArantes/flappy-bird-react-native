243ef95046ab234c8d903aad4068de5795e7ce4f
Leon Arantes <64018468+LeonArantes@users.noreply.github.com>
2021-06-01 21:16:19 -0300

    chore: initialize project

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={{flex: 1, backgroundColor: 'red'}}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+};
+
+export default App;
