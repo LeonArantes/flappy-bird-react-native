f21f0da7c410d46c368461aa45c53e87438178e4
Leon Arantes <64018468+LeonArantes@users.noreply.github.com>
2021-06-13 22:18:48 -0300

    chore: rename project

--- ios/FlappyBirdTests/FlappyBirdTests.m
+++ ios/FlappyBirdTests/FlappyBirdTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface FlapBigouTests : XCTestCase
+@interface FlappyBirdTests : XCTestCase
 
 @end
 
-@implementation FlapBigouTests
+@implementation FlappyBirdTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
